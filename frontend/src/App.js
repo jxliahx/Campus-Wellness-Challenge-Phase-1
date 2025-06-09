0ee838a31ff999de083c14bd2dc1ffb2a6425aad
Julia <jxlia.hx@gmail.com>
2025-06-08 19:50:31 -0600

    created homepage using materialui, created css file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Header from './components/Header'
 import Dashboard from './pages/P_Dashboard'
 import Login from './pages/B_Login'
 import Register from './pages/P_Register'
+import Homepage from './pages/B_Homepage'
+
+function AppContent() {
+  const location = useLocation();
+  const isHomepage = location.pathname === '/';
+
+  return (
+    <div className='container'>
+      {!isHomepage && <Header />}
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </div>
+  );
+}
 
 function App() {
   return (
     <>
       <Router>
-        <div className='container'>
-          <Header />
-          <Routes>
-            <Route path='/' element={<Dashboard />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-          </Routes>
-        </div>
+        <AppContent />
       </Router>
       <ToastContainer />
     </>
--- frontend/src/pages/B_Homepage.jsx
+++ frontend/src/pages/B_Homepage.jsx
@@ -6,3 +6,56 @@
     File Name: B_Homepage.jsx
     For: Both coordinator and participants
 */
+
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { 
+  Container, 
+  Typography, 
+  Button, 
+  Box,
+  Paper
+} from '@mui/material';
+import '../styles/pages.css';
+
+function Homepage() {
+  const navigate = useNavigate();
+
+  return (
+    <Container component="main" maxWidth="sm" className="page-container">
+      <Paper className="homepage-container">
+        <Typography 
+          component="h1" 
+          variant="h2" 
+          className="welcome-text"
+        >
+          Welcome to Campus Wellness Challenge!
+        </Typography>
+        
+        <Box className="button-container">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => navigate('/login')}
+            size="large"
+            className="primary-button"
+          >
+            Login
+          </Button>
+          
+          <Button
+            variant="outlined"
+            color="primary"
+            onClick={() => navigate('/register')}
+            size="large"
+            className="secondary-button"
+          >
+            Sign Up
+          </Button>
+        </Box>
+      </Paper>
+    </Container>
+  );
+}
+
+export default Homepage;
