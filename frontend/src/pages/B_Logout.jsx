e099cafd02f9ac0fb2f17d053dfae699136758fd
annachuu <annachu211@gmail.com>
2025-06-11 23:10:36 -0600

    create logout page and linked it

--- frontend/src/pages/B_Logout.jsx
+++ frontend/src/pages/B_Logout.jsx
@@ -7,51 +7,66 @@
 //     For: Both coordinator and participants
 // */
 
-// import React from 'react'
-// import { 
-//   Container, 
-//   Typography, 
-//   Button, 
-//   Box,
-//   Paper
-// } from '@mui/material'
-// import { useDispatch, userSelector } from 'react-redux'
-// import { logout, reset } from '../features/auth/authSlice'
-// import { useNavigate } from 'react-router-dom'
-// import CloseIcon from '@mui/icons-material/Close'
-// import '../styles/pages.css';
+import React from 'react'
+import { 
+  Container, 
+  Typography, 
+  Button, 
+  Box,
+  Paper,
+  IconButton
+} from '@mui/material'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout, reset } from '../features/auth/authSlice'
+import { useNavigate } from 'react-router-dom'
+import CloseIcon from '@mui/icons-material/Close'
+import WavingHandIcon from '@mui/icons-material/WavingHand'
+import '../styles/pages.css';
 
-// function Logout() {
-//     const dispatch = useDispatch()
-//     const navigate = useNavigate()
-//     const {user} = useSelector(/*(state) => state.auth*/)
+function Logout() {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const {user} = useSelector((state) => state.auth)
 
-//     const handleConfirmLogout = () => {
-//         dispaych(logout())
-//         dispatch(reset())
-//         navigate('/')
-//     }
+    const handleConfirmLogout = () => {
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/')
+    }
 
-//     const handleCanel = () => {
-//         navigate('/')
-//     }
+    const handleCancel = () => {
+        navigate('/participant-dashboard')                      //works for coordinator even though says participant?
+    }
 
-//     return (
-//         <Box
-//             sx={{
-//                 height: '100vh', 
-//                 display: 'flex', 
-//                 justifyContent: 'center', 
-//                 alignItems: 'center', 
-//                 backgroundColor: '#f5f5f5',
-//                 }}
-//         >
-//             <Paper elevation={3}
-//             >
+    return (
+        <Box
+            sx={{
+                height: '100vh', 
+                display: 'flex', 
+                justifyContent: 'center', 
+                alignItems: 'center', 
+                backgroundColor: '#f5f5f5',
+                }}
+        >
+            <Paper elevation={4} sx={{position: 'relative', p: 10, width: 700, height: 600, textAlign: 'center'}}>
+                {/* X Icon */}
+                <IconButton onClick={handleCancel} sx={{position: 'absolute', top: 30, right: 30}}>
+                    <CloseIcon sx={{fontSize: 40}}/>
+                </IconButton>
 
-//             </Paper>
-//         </Box>
-//     )
-// }
-// 
-// export default Logout
+                {/* Hand Icon */}
+                <WavingHandIcon sx={{fontSize: 100, mt: 9, mb: 2}}/>
+
+                <Typography sx={{fontSize: 35}}>
+                    Bye! {user?.name || 'User'}
+                </Typography>
+
+                <Button variant="contained" color="primary" onClick={handleConfirmLogout} fullWidth sx={{mt: 6, height: 55, width: 180}}>
+                    Confirm Logout
+                </Button>
+            </Paper>
+        </Box>
+    )
+}
+
+export default Logout
